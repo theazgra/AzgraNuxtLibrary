63faa2a50b1230a0c38c9525a5f8c1a554b0d46c
Vojtěch Moravec <theazgra@gmail.com>
2022-07-21 12:10:32 +0200

    New packaves

--- src/components/button.vue
+++ src/components/button.vue
@@ -4,22 +4,57 @@
     style="padding: 15px; background-color: red; width: 50%"
   >
     <button @click="btnClick" style="color: blue">{{ title }}</button>
-    <!-- <Select /> -->
+    <ElSelect v-model="value" placeholder="Select value ...">
+      <ElOption
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </ElSelect>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { notificationService } from "../services/notification";
-// import { Select } from "element-ui";
+import { Select as ElSelect, Option as ElOption } from "element-ui";
+
 export default Vue.extend({
   name: "AzgraButton",
-  // components: { Select },
+  components: { ElSelect, ElOption },
   props: {
     title: {
       type: String,
       required: true,
     },
+  },
+  data() {
+    return {
+      options: [
+        {
+          value: "Option1",
+          label: "Option1",
+        },
+        {
+          value: "Option2",
+          label: "Option2",
+        },
+        {
+          value: "Option3",
+          label: "Option3",
+        },
+        {
+          value: "Option4",
+          label: "Option4",
+        },
+        {
+          value: "Option5",
+          label: "Option5",
+        },
+      ],
+      value: "",
+    };
   },
   methods: {
     btnClick() {
@@ -32,4 +67,8 @@ export default Vue.extend({
  <style lang="sass" scoped>
 .azgra__button
   height: 250px
+  button
+    background-color: none
+    padding: 12px
+    border: 5px solid blue
 </style>
---
+++ src/components/clicker.vue
@@ -0,0 +1,13 @@
+<template>
+  <ElButton>Click me!</ElButton>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Button as ElButton } from "element-ui";
+
+export default Vue.extend({
+  name: "Clicker",
+  components: { ElButton },
+});
+</script>
