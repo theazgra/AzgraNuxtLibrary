7350159c80424c38fd6e87a90fa18e7a3610e1fe
Vojtěch Moravec <theazgra@gmail.com>
2022-07-21 08:55:55 +0200

    Simple nuxt component lib.

---
+++ nuxt/index.ts
@@ -0,0 +1,15 @@
+import { join } from "path";
+
+export default function() {
+    const { nuxt } = this;
+    if (!nuxt.options.components) {
+        throw new Error('Nuxt components are not enabled.');
+    }
+
+    this.nuxt.hook('components:dirs', (dirs) => {
+        dirs.push({
+            path: join(__dirname, '../src/components'),
+            prefix: 'azgra'
+        })
+    });
+}
